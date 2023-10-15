15e165c6f80f1f5113a890d9181e7acf566434b5
nicodemosmello <nicodemusgimello21@gmail.com>
2023-10-14 22:32:48 -0300

    testando commandos

---
+++ rubyteste1.rb
@@ -0,0 +1,49 @@
+# praticnado comandos
+print "Digite seu nome: "
+nome = gets.chomp
+print "Agora ditige sua idade: "
+idade = gets.chomp.to_i
+
+puts "Olá #{nome}, tudo bem? Sua idade é: #{idade}. Obrigado "
+
+
+=begin praticando comentários que explicam o que foi feito,
+onde neste caso é utilizado para comentários que exigem várias linhas
+de textos sem precisar ficar digitanto # no início das
+das linhas.
+=end
+
+# Dados numéricos
+print "x = 1"
+x = 1
+# integer value
+print "y=1.2"
+y=1.2
+y.class
+# float value
+print 1+3
+# addition: output 4
+print 4-1
+#subtraction output 3
+
+var = 'paisagem'
+print var.length
+print var.reverse
+print var.upcase
+
+#strings
+s1 = "São sequência de caracteres que pode ser em aspas duplas"
+s2 = 'ou em aspas únicas'
+year2023 = true
+year2000 = false
+
+# MAIÚSCULAS E minúsculas
+puts "upcase".upcase
+puts 'dowcase'. downcase
+
+#interpolação (vou utilizar nos códigos acima) = isere códigos dentre de uma cadeia de caracteres
+
+
+
+
+
